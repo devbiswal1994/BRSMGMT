165001765272874c033c651a5ed49b83b532b78e
ASUS <ASUS@DESKTOP-1C7HRA5.bbrouter>
2022-01-24 20:10:09 +0530

    First commit

---
+++ src/com/dev/model/Employee.java
@@ -0,0 +1,30 @@
+package com.dev.model;
+
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	private Double empSalary;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(Double empSalary) {
+		this.empSalary = empSalary;
+	}
+	
+	
+
+}
