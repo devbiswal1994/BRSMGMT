b9bbbf9cd65a918cf9dd1ae36870e57c4a3f2301
ASUS <ASUS@DESKTOP-1C7HRA5.bbrouter>
2022-01-24 20:22:44 +0530

    2nd commit

---
+++ src/com/dev/model/Student.java
@@ -0,0 +1,29 @@
+package com.dev.model;
+
+public class Student {
+	
+	private int studentId;
+	private String studentName;
+	private Double studentSalary;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public Double getStudentSalary() {
+		return studentSalary;
+	}
+	public void setStudentSalary(Double studentSalary) {
+		this.studentSalary = studentSalary;
+	}
+	
+	
+
+}
