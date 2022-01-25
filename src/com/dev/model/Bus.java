95fcef142a61651105f2823a471b2b239f111ef0
ASUS <ASUS@DESKTOP-1C7HRA5.bbrouter>
2022-01-25 15:46:01 +0530

    Variables and methods added to bus class

--- src/com/dev/model/Bus.java
+++ src/com/dev/model/Bus.java
@@ -2,5 +2,21 @@ package com.dev.model;
 
 public class Bus {
 	
+	private int busNo;
+	private String busName;
+	
+	public int getBusNo() {
+		return busNo;
+	}
+	public void setBusNo(int busNo) {
+		this.busNo = busNo;
+	}
+	public String getBusName() {
+		return busName;
+	}
+	public void setBusName(String busName) {
+		this.busName = busName;
+	}
+	
 
 }
