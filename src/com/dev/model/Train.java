8cef09b6b4bb96445a130cc1d9692ac2e7306ed2
ASUS <ASUS@DESKTOP-1C7HRA5.bbrouter>
2022-01-25 15:54:27 +0530

    Properties added to Train class

--- src/com/dev/model/Train.java
+++ src/com/dev/model/Train.java
@@ -1,5 +1,22 @@
 package com.dev.model;
 
 public class Train {
+	
+	private int trainNo;
+	private String trainName;
+	public int getTrainNo() {
+		return trainNo;
+	}
+	public void setTrainNo(int trainNo) {
+		this.trainNo = trainNo;
+	}
+	public String getTrainName() {
+		return trainName;
+	}
+	public void setTrainName(String trainName) {
+		this.trainName = trainName;
+	}
+	
+	
 
 }
